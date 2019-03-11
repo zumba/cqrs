e39543787ad28cee021691fe44ab678b8559218b
Stephen Young <young.steveo@gmail.com>
2019-03-11 11:16:36 -0400

    Added Command bus tests

---
+++ test/Command/CommandBusTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Zumba\Test\CQRS\Command;
+
+use \Zumba\CQRS\Command\Command,
+	\Zumba\CQRS\Command\CommandBus,
+	\Zumba\CQRS\Command\CommandResponse,
+	\Zumba\CQRS\Response;
+
+/**
+ * @group cqrs
+ * @group command
+ */
+class CommandBusTest extends \Zumba\Service\Test\TestCase {
+	public function testDispatch() {
+		$dtoBus = $this->getMockBuilder(\Zumba\CQRS\Bus::class)->disableOriginalConstructor()->getMock();
+		$response = $this->getMockBuilder(CommandResponse::class)->disableOriginalConstructor()->getMock();
+		$command = $this->getMockBuilder(Command::class)->getMock();
+		$dtoBus
+			->expects($this->once())
+			->method('dispatch')
+			->with($command)
+			->will($this->returnValue($response));
+
+		$bus = CommandBus::fromBus($dtoBus);
+		$this->assertSame($response, $bus->dispatch($command));
+	}
+
+	/**
+	 * @expectedException \Zumba\CQRS\InvalidResponse
+	 */
+	public function testDispatchFailed() {
+		$dtoBus = $this->getMockBuilder(\Zumba\CQRS\Bus::class)->disableOriginalConstructor()->getMock();
+		$response = $this->getMockBuilder(Response::class)->getMock();
+		$command = $this->getMockBuilder(Command::class)->getMock();
+		$dtoBus
+			->expects($this->once())
+			->method('dispatch')
+			->with($command)
+			->will($this->returnValue($response));
+
+		$bus = CommandBus::fromBus($dtoBus);
+		$bus->dispatch($command);
+	}
+}

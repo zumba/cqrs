8081b30cb9fba31ae997fae0737f49aab6df25bf
Stephen Young <young.steveo@gmail.com>
2019-03-11 11:08:11 -0400

    Test coverage for failure.

---
+++ test/Command/Response/FailureTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Zumba\Test\CQRS\Command\Response;
+
+use \Zumba\CQRS\Command\CommandResponse,
+	\Zumba\CQRS\Command\Response\Failure;
+
+/**
+ * @group cqrs
+ * @group command
+ */
+class FailureTest extends \Zumba\Service\Test\TestCase {
+	public function testError() {
+		$e = new \Exception('test');
+		$response = CommandResponse::fail($e);
+		$this->assertSame($e, $response->error());
+	}
+}

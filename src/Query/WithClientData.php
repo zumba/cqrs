266ad83dc1d2744fff538981f27a112bfbe3ef64
Stephen Young <young.steveo@gmail.com>
2019-07-09 15:10:15 -0400

    Remove magic merging of client data into property data. Everything is handled by seperate interfaces now.

---
+++ src/Query/WithClientData.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+namespace Zumba\CQRS\Query;
+
+use \Zumba\CQRS\ClientData;
+
+interface WithClientData {
+
+	/**
+	 * Create a Query from a ClientData object
+	 */
+	public static function fromClientData(ClientData $clientData) : Query;
+}

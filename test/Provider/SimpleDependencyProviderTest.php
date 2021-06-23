1f11c1e0dd7cc479a14c1dbf0484ef7da88c1bc4
Jonathan Foote <jonathan.foote@zumba.com>
2021-06-23 10:22:19 -0500

    Update test to ignore deprecation warning for now until we can address, so it passes in PHP8

--- test/Provider/SimpleDependencyProviderTest.php
+++ test/Provider/SimpleDependencyProviderTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Zumba\CQRS\Test\Provider;
 
 use PHPUnit\Framework\TestCase;
+use Zumba\CQRS\Provider\InvalidDependency;
 use Zumba\CQRS\Provider\SimpleDependencyProvider;
 use Zumba\CQRS\Test\Fixture\SimpleDependencyProvider\EmptyConstructorCommand;
 use Zumba\CQRS\Test\Fixture\SimpleDependencyProvider\EmptyConstructorCommandHandler;
@@ -17,32 +18,48 @@ use Zumba\CQRS\Test\Fixture\SimpleDependencyProvider\PrivateConstructorCommand;
 class SimpleDependencyProviderTest extends TestCase
 {
     /**
-     * @expectedException \Zumba\CQRS\Provider\InvalidDependency
+     * @var int
      */
+    private $existingErrorLevel;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->existingErrorLevel = error_reporting();
+        // for now, ignore deprecated errors: to avoid error in most of the tests:
+        // Method ReflectionParameter::getClass() is deprecated
+        // todo: refactor to not use getClass anymore, then remove this work-around
+        error_reporting($this->existingErrorLevel & ~E_DEPRECATED);
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        // restore original error reporting level
+        error_reporting($this->existingErrorLevel);
+    }
+
     public function testNonOptionalParam(): void
     {
         $provider = new SimpleDependencyProvider();
         $dto = new NonOptionalCommand();
+        $this->expectException(InvalidDependency::class);
         $provider->getCommandHandler($dto);
     }
 
-    /**
-     * @expectedException \Zumba\CQRS\Provider\InvalidDependency
-     */
     public function testNonInstantiable(): void
     {
         $provider = new SimpleDependencyProvider();
         $dto = new PrivateConstructorCommand();
+        $this->expectException(InvalidDependency::class);
         $provider->getCommandHandler($dto);
     }
 
-    /**
-     * @expectedException \Zumba\CQRS\Provider\InvalidDependency
-     */
     public function testNonValidCommandHandler(): void
     {
         $provider = new SimpleDependencyProvider();
         $dto = new NotValidCommand();
+        $this->expectException(InvalidDependency::class);
         $provider->getCommandHandler($dto);
     }
 
